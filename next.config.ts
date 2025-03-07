2f708f63a1c205c482ce556db4330096c30887a7
betselotT <betselottesfa9@gmail.com>
2025-03-06 21:50:20 -0900

    practicing mesh

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
